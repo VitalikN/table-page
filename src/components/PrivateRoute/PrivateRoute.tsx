b5f6edfc0bf2723c2028b0a84dd9ce3ede68a16c
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2023-10-25 19:07:50 +0300

    fix console

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -2,27 +2,20 @@
 
 import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { IProps, RootState } from "../../types/types";
-import { Balls } from "../Balls";
 
 const PrivateRoute: React.FC<IProps> = ({ children }) => {
   const router = useRouter();
   const token = useSelector((state: RootState) => state.auth?.token);
-  const [loading, setLoading] = useState(true);
 
-  console.log("Token:", token);
   useEffect(() => {
     if (!token) {
       router.replace("/login");
     } else {
-      setLoading(false);
     }
   }, [router, token]);
 
-  //   if (loading) {
-  //     return <Balls />;
-  //   }
   return <>{children}</>;
 };
 
